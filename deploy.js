9ac3458873b06b0c84b6cba39b37f18b5e6f9f7a
AkshitV <akshitvaid@gmail.com>
2018-07-23 07:11:52 +0530

    Add files via upload

---
+++ deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require ('./compile');
+
+const provider = new HDWalletProvider(
+	'pelican guard book woman denial axis people track pride swift gentle atom',
+	'https://rinkeby.infura.io/6tyZSrcywaJSoeSlchNs'
+);
+const web3 = new Web3(provider);
+//use async,await syntax multiple times
+
+const deploy = async () => {
+	const accounts = await web3.eth.getAccounts();
+
+	console.log('attempting to deploy', accounts[0]);
+	
+	const result = await new web3.eth.Contract(JSON.parse(interface))//abi
+		.deploy({data: bytecode, arguments : ['hi there'] })
+		.send({gas: '1000000', from: accounts[0]});
+	// to record where our contract got deployed?
+	
+	console.log('contract deployed to - ',result.options.address);
+};
+
+deploy();
